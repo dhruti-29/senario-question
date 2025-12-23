6aabad95382919eca1bcc2805a8f98695e462e2a
dhruti viradiya <25cs113@charusat.edu.in>
2025-12-23 23:03:15 +0530

    add new small project
    add new small project

---
+++ Hospital Patient Records.c
@@ -0,0 +1,48 @@
+/*A health-monitoring startup stores step-count logs for every user.
+Each user has a small note written by the AI coach, an array storing
+ step counts for different time slots of the day,
+and a dynamically allocated ID-code that the system generates during 
+login.
+The company wants a function that reads multiple users’ records, 
+verifies whether any step count is abnormally low,
+processes the coach-notes to remove extra spaces, and prints only 
+those users whose generated ID-code length is valid.
+Write a C program using functions + arrays + malloc/calloc + strings 
+to perform all these checks.*/
+
+#include <stdio.h>
+
+
+struct ai{ 
+char note[1000];
+    int array[5];
+    int id;
+}s1;
+
+struct read(struct ai s1);
+ count
+int main() {
+
+    int *p=&id.s1;
+    p =(int *)malloc(id * sizeof(id.s1));
+struct read();
+
+    return 0;
+}
+struct read(struct ai s1){
+
+    printf("enter note :");
+    fgets(s1.note,sizeof(s1.note),stdin);
+printf("enter stepcunt :");
+    for(int i=0;i<5;i++){
+scanf("%d",a1[i].array);
+    }
+    printf("enter id :");
+    scanf("%d",&a1.id);
+    
+
+
+     
+
+    
+}
