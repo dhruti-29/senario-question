8da8fa3bc85317bb5280d38cb6e49753b447ec9d
dhruti viradiya <25cs113@charusat.edu.in>
2026-01-02 23:19:32 +0530

    add new file

---
+++ Structure _inside _function.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+//structure inside function means we write structure synatx inside function 
+ void structure(){
+        struct meta{
+            char name[100];
+            int id;
+        };
+        struct meta n1;
+printf("enter name : ");
+scanf("%s",n1.name);
+printf("enter id : ");
+scanf("%d",&n1.id);
+
+
+    }
+/////////////////////////////
+    int main(){ 
+structure();
+
+    }
+    
