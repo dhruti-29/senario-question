7413bdad3c1a22cf45ae3b1a3eb2bb97e67f3678
dhruti viradiya <25cs113@charusat.edu.in>
2026-01-06 19:32:43 +0530

    add new

---
+++ 2 number sum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int ssum(int a){
+  int b,sum=0;
+  printf("Enter your number: ");
+  scanf("%d",&a);
+
+  printf("Enter your number: ");
+  scanf("%d",&b);
+
+return a+b;
+}
+
+int main(){
+int a,b;
+
+printf("%d",ssum(a+b));
+
+
+}
