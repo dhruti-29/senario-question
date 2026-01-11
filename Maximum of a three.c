664ab41b1d0efa812ca909d72cc24f72b3737759
dhruti viradiya <25cs113@charusat.edu.in>
2026-01-11 13:15:58 +0530

    add file

---
+++ Maximum of a three.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int max(int a,int b,int c){
+    if(a>b && a>c) return a;
+    if(b>c) return b;
+    return c;
+}
+
+int main(){
+    printf("%d", max(3,7,5));
+    return 0;
+}
