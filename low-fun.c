5147c1f6473a3c056b27d04937b1e64ae77f49bc
dhruti viradiya <25cs113@charusat.edu.in>
2026-01-21 21:22:34 +0530

    add new c file

---
+++ low-fun.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char a[100];
+    int i;
+
+    printf("Enter string: ");
+    gets(a);
+
+    for(i = 0; a[i] != '\0'; i++) {
+        if(a[i] >= 'A' && a[i] <= 'Z') {
+            a[i] = a[i] + 32;  
+        }
+    }
+
+    printf("Lowercase = %s", a);
+
+    return 0;
+}
