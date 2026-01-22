17d1e52da309fbd41bd548c8287f22b60f812445
dhruti viradiya <25cs113@charusat.edu.in>
2026-01-22 22:05:48 +0530

    add new c file.

---
+++ uppercas.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char a[100];
+
+    printf("Enter string: ");
+    gets(a);
+
+    for(int i = 0; a[i] != '\0'; i++) {
+
+        
+        if(a[i] >= 'a' && a[i] <= 'z') {
+            a[i] = a[i] - 32;
+        }
+    }
+
+    printf("Uppercase = %s", a);
+
+    return 0;
+}
